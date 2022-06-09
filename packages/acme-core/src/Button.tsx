ec3493eb85c16ad29948bbee580953b849cd0fc3
Adam Mescher <admescher@gmail.com>
2022-06-09 15:06:32 -0600

    Update button styles and add Styled Components for Chromatic onboarding hell

--- packages/acme-core/src/Button.tsx
+++ packages/acme-core/src/Button.tsx
@@ -1,11 +1,19 @@
-import * as React from "react";
+import * as React from 'react';
+import styled from 'styled-components';
 
 export interface ButtonProps {
   children: React.ReactNode;
 }
 
+const Base = styled.button`
+  border: 2px solid;
+  background: none;
+  color: inherit;
+  padding: 0.5rem 1rem;
+`;
+
 export function Button(props: ButtonProps) {
-  return <button>{props.children}</button>;
+  return <Base>{props.children}</Base>;
 }
 
-Button.displayName = "Button";
+Button.displayName = 'Button';
